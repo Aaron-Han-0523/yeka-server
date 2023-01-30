2802a2f04b0e92947e94c0ddba64492bb00d59f5
aaronhan0523@gmail.com <aaronhan0523@gmail.com>
2023-01-30 15:48:05 +0900

    YEKA-012 aaron hme화면 커뮤니티 공지사항 글 제외

--- controllers/community.js
+++ controllers/community.js
@@ -1,3 +1,4 @@
+const { sequelize } = require("../models");
 const db = require("../models");
 const Community = db.community;
 const Op = db.Sequelize.Op;
@@ -38,8 +39,7 @@ exports.create = (req, res) => {
 
 // Retrieve all Communities from the database.
 exports.findAll = (req, res) => {
-  const title = req.query.title;
-  var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
+  var condition = { community_type: { [Op.ne]: 0 } };
 
   Community.findAll({ where: condition, limit: 4, order: [["id", "DESC"]] })
     .then((data) => {
