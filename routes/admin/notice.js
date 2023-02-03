411934e9f348e175395cfa5651c7342b2e36142a
Mu-jun <suo24691@gmail.com>
2023-02-03 14:01:20 +0900

    YEKA-XXX miracle 공지사항 & 자유게시판 이미지 출력

--- routes/admin/notice.js
+++ routes/admin/notice.js
@@ -1,5 +1,6 @@
 module.exports = app => {
   const notice = require("../../controllers/admin/notice.js");
+  const fileParser = require("../../utils/fileParser");
 
   var router = require("express").Router();
 
@@ -9,11 +10,19 @@ module.exports = app => {
 
   router.get("/detail/:id", notice.findOne);
 
-  router.post("/add", notice.create);
+  router.post("/add", fileParser.upload("notice").single("image"), path_injection, notice.create);
 
-  router.post("/detail/:id", notice.update);
+  router.post("/detail/:id", fileParser.upload("notice").single("image"), path_injection, notice.update);
 
   router.get("/delete/:id", notice.delete);
 
   app.use('/admin/notice', router);
+
+  function path_injection(req, res, next) {
+    console.log("notice file :", req.file);
+    if (req.file) {
+      req.body.community_link = req.file.path;
+    }
+    next()
+  }
 };
